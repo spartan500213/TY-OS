0b3205a60fb7dfd269c2649e466880bd31824fb4
your name <you@example.com>
2025-08-12 11:58:40 +0530

    week 4 assignments

---
+++ w4/b.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void b() {
+	printf("hello from b()\n");
+	return;
+}
---
+++ w4/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void a();
+void b();
+
+int main() {
+	printf("hello from main()\n");
+	a();
+	b();
+	return 0;
+}
